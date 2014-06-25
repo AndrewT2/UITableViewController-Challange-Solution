a1d462938c6af101003c3a72fa6aac6dc70f003e
Andrew Tweed <andrewtweed@Andrew-Mac.local>
2014-06-25 15:44:49 +0100

    added UITVC controller class

--- UITVC Challenge.h
+++ UITVC Challenge.h
@@ -1,13 +1,13 @@
 //
-//  CCViewController.h
+//  UITVC Challenge.h
 //  UITableViewController Challenge Solution
 //
-//  Created by Andrew Tweed on 24/06/2014.
+//  Created by Andrew Tweed on 25/06/2014.
 //  Copyright (c) 2014 Andrew Tweed. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface UITVC_Challenge : UITableViewController
 
 @end
